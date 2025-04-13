ff7a312fc356be833eeff27fd7ae22667bf44590
nkdyo454 <navalkishordewangan454@gmail.com>
2025-04-13 20:46:54 +0530

    Update webserver_config.py

--- webserver_config.py
+++ webserver_config.py
@@ -1,24 +1,24 @@
-      import os
-      from flask_appbuilder.security.manager import AUTH_OAUTH
-      AUTH_TYPE = AUTH_OAUTH
-      AUTH_ROLES_SYNC_AT_LOGIN = True
-      AUTH_USER_REGISTRATION = True
-      AUTH_USER_REGISTRATION_ROLE = "Admin"
-      OAUTH_PROVIDERS = [
-          {
-              'name': 'okta', 'icon': 'fa-circle-o',
-              'token_key': 'access_token',
-              'remote_app': {
-                  'client_id': '0oao3wrk7aga2Lj7b5d7',  # Replace with your Okta client ID
-                  'client_secret': '5MpP2ReCfHZDMjEqK0lyGh4UzdkuaSbLaU8zbAweKCRrlpjFRlBTHrjyFVfPTZnc',  # Replace with your Okta client secret
-                  'api_base_url': 'https://dev-70294059.okta.com/oauth2/default/v1/',  # Replace with your Okta domain
-                  'client_kwargs': {
-                      'scope': 'openid profile email groups'
-                  },
-                  'access_token_url': 'https://dev-70294059.okta.com/oauth2/default/v1/token',  # Replace with your Okta domain
-                  'authorize_url': 'https://dev-70294059.okta.com/oauth2/default/v1/authorize',# Replace with your Okta domain
-                  'jwks_uri': "https://myoktadomain.okta.com/oauth2/v1/keys"
+      # import os
+      # from flask_appbuilder.security.manager import AUTH_OAUTH
+      # AUTH_TYPE = AUTH_OAUTH
+      # AUTH_ROLES_SYNC_AT_LOGIN = True
+      # AUTH_USER_REGISTRATION = True
+      # AUTH_USER_REGISTRATION_ROLE = "Admin"
+      # OAUTH_PROVIDERS = [
+      #     {
+      #         'name': 'okta', 'icon': 'fa-circle-o',
+      #         'token_key': 'access_token',
+      #         'remote_app': {
+      #             'client_id': '0oao3wrk7aga2Lj7b5d7',  # Replace with your Okta client ID
+      #             'client_secret': '5MpP2ReCfHZDMjEqK0lyGh4UzdkuaSbLaU8zbAweKCRrlpjFRlBTHrjyFVfPTZnc',  # Replace with your Okta client secret
+      #             'api_base_url': 'https://dev-70294059.okta.com/oauth2/default/v1/',  # Replace with your Okta domain
+      #             'client_kwargs': {
+      #                 'scope': 'openid profile email groups'
+      #             },
+      #             'access_token_url': 'https://dev-70294059.okta.com/oauth2/default/v1/token',  # Replace with your Okta domain
+      #             'authorize_url': 'https://dev-70294059.okta.com/oauth2/default/v1/authorize',# Replace with your Okta domain
+      #             'jwks_uri': "https://myoktadomain.okta.com/oauth2/v1/keys"
 
-              }
-          }
-      ]
+      #         }
+      #     }
+      # ]
